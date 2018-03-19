d56e0078daaa4f404ef142879c1f30a80542de03
Ben Lei <ben181231@gmail.com>
2018-03-20 01:21:47 +0800

    Add a dummy server in dispatcher

--- cmd/dispatcher/dispatcher.go
+++ cmd/dispatcher/dispatcher.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/ALSAD-project/alsad-core/pkg/dispatcher/config"
 	"github.com/kelseyhightower/envconfig"
@@ -10,8 +12,19 @@ import (
 func main() {
 	dpConfig := config.Config{}
 	if err := envconfig.Process("dp", &dpConfig); err != nil {
-		log.Fatalf("Error on processing configuration - %s", err.Error())
+		log.Fatalf("Error on processing configuration: %s", err.Error())
+		return
 	}
 
-	log.Printf("Config: %v", dpConfig)
+	mux := http.NewServeMux()
+	// TODO: add api handlers
+	mux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		http.NotFound(resp, req)
+	})
+
+	log.Printf("Server is listening on port %d", dpConfig.Port)
+	log.Fatal(http.ListenAndServe(
+		fmt.Sprintf(":%d", dpConfig.Port),
+		mux,
+	))
 }
