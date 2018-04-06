823ffdbd633e114304c87dfede4abfbeaffd2122
Ben Lei <ben181231@gmail.com>
2018-04-06 22:36:31 +0800

    Update config of dispatcher

--- pkg/dispatcher/config/config.go
+++ pkg/dispatcher/config/config.go
@@ -2,13 +2,17 @@ package config
 
 // Config defines the configuration of a dispatcher
 type Config struct {
-	Port              int    `envconfig:"PORT" default:"8000"`
-	EnterMode         string `split_words:"true" required:"true"`
-	ExpertDb          string `split_words:"true" required:"true"`
-	ExpertDbInputDir  string `split_words:"true" required:"true"`
-	ExpertDbOutputDir string `split_words:"true" required:"true"`
-	FeederURL         string `envconfig:"FEEDER_URL" required:"true"`
-	BaURL             string `envconfig:"BA_URL" required:"true"`
-	UslURL            string `envconfig:"USL_URL" required:"true"`
-	SlURL             string `envconfig:"SL_URL" required:"true"`
+	Port int `envconfig:"PORT" default:"8000"`
+
+	EnterMode string `split_words:"true" required:"true"`
+
+	FsqRedisAddr         string `split_words:"true" required:"true"`
+	FsqDir               string `split_words:"true" required:"true"`
+	FsqExpertInputQueue  string `split_words:"true" required:"true"`
+	FsqExpertOutputQueue string `split_words:"true" required:"true"`
+
+	FeederUrl string `split_words:"true" required:"true"`
+	BaUrl     string `split_words:"true" required:"true"`
+	UslUrl    string `split_words:"true" required:"true"`
+	SlUrl     string `split_words:"true" required:"true"`
 }
