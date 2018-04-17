9ff525e9aafb58f8f51d515901138327daa4b76d
Forte <forte@ieee.org>
2018-04-17 11:24:56 +0800

    code refratoring

--- cmd/learners/drivers/main.go
+++ cmd/learners/drivers/main.go
@@ -15,11 +15,11 @@ import (
 )
 
 func signalHandler(cmd *exec.Cmd) {
-    signal_chan := make(chan os.Signal, 1)
-    signal.Notify(signal_chan, syscall.SIGINT)
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, syscall.SIGINT)
     go func() {
         for {
-            s := <-signal_chan
+            s := <-signalChan
             switch s {
             case syscall.SIGINT:
                 if err := cmd.Process.Kill(); err != nil {
@@ -31,9 +31,9 @@ func signalHandler(cmd *exec.Cmd) {
     }()
 }
 
-func handleStreamIn(userProgConn net.Conn, inputConn net.Conn) {
+func handleStreamIn(dataSourceConn net.Conn, userProgConn net.Conn) {
     for {
-        streamIn, err := bufio.NewReader(inputConn).ReadString('\n')
+        streamIn, err := bufio.NewReader(dataSourceConn).ReadString('\n')
         if err != nil {
             panic(err)
         }
@@ -41,48 +41,57 @@ func handleStreamIn(userProgConn net.Conn, inputConn net.Conn) {
     }
 }
 
-func handleStreamOut(userProgConn net.Conn, inputConn net.Conn) {
+func handleStreamOut(dataSourceConn net.Conn, userProgConn net.Conn) {
     for {
         streamOut, err := bufio.NewReader(userProgConn).ReadString('\n')
         if err != nil {
             panic(err)
         }
-        fmt.Fprintf(inputConn, streamOut)
+        fmt.Fprintf(dataSourceConn, streamOut)
     }
 }
 
 func main() {
 
+    // Load ENV configurations
     driverConfig := config{}
     if err := envconfig.Process("driver", &driverConfig); err != nil {
         log.Fatalf("Error on processing configuration: %s", err.Error())
         return
     }
 
-    args := strings.Split(driverConfig.UserProgramArgs, " ")
-    cmd := exec.Command(driverConfig.UserProgram, args...)
+    // Run user program
+    UserProg := strings.Split(driverConfig.UserProgram, " ")
+    cmd := exec.Command(UserProg[0], UserProg[1:]...)
     if err := cmd.Start(); err != nil {
         panic(err)
     }
+    // Create signal handler for killing user program when ^C received
     go signalHandler(cmd)
 
-    userProgConn, err := net.Dial("tcp", driverConfig.StreamInURL)
+    // Connect to stream in data source
+    dataSourceConn, err := net.Dial("tcp", driverConfig.StreamInURL)
     if err != nil {
         panic(err)
     }
     
+    // Listen for stream out data target (i.e. user program)
     ln, err := net.Listen("tcp", driverConfig.StreamOutURL)
     if err != nil {
         panic(err)
     }
 
+    // Always wait for user program(s) to connect
     for {
-        inputConn, err := ln.Accept()
+        // Once connected by a user program,
+        userProgConn, err := ln.Accept()
         if err != nil {
             panic(err)
         }
-        go handleStreamIn(userProgConn, inputConn)
-        go handleStreamOut(userProgConn, inputConn)
+        // pass stream in data to user program, and
+        go handleStreamIn(dataSourceConn, userProgConn)
+        // pass stream out data to data source. 
+        go handleStreamOut(dataSourceConn, userProgConn)
     }
 
 }
